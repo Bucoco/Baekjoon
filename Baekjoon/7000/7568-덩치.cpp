635611818046893535b27f632506ecd2c0f4e89c
윤호 남 <nam2660433@naver.com>
2021-03-19 16:57:13 +0900

    Solve 7568

--- Baekjoon/7000/7568-덩치.cpp
+++ Baekjoon/7000/7568-덩치.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
+#include <utility>
+#include <vector>
 using namespace std;
 
 int main() {
 	cin.tie(0); cout.tie(0);
 	ios::sync_with_stdio(false);
 
+	vector<pair<int, int>> v;
+	vector<int> ans;
+	int x, y, n;
 
+	cin >> n;
+	v.resize(n);
+	ans.resize(n);
 
+	for (auto& i : v) {
+		cin >> i.first >> i.second;
+	}
 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (v[i].first < v[j].first && v[i].second < v[j].second)
+				ans[i]++;
+		}
+	}
 
+	for (int i : ans)
+		cout << i + 1 << ' ';
 
 	return 0;
 }
